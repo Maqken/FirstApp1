9cf0acada9353ff2a3e22f199de3a5f757b81fbb
Juan Vidal <juan@conversalab.com>
2018-10-04 14:08:38 -0300

    Estado al 04/10/2018

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function GetAndShowLiveSuscriptions() {
             })
             console.log(commaSepChannelsIds);
             console.log(response.follows);
-            var streamRequest = new XMLHttpRequest();
+            /*var streamRequest = new XMLHttpRequest();
                 streamRequest.onreadystatechange = function(){
                     if (this.readyState == 4 && this.status == 200){
                         var streamResponse = JSON.parse(this.responseText);
@@ -29,7 +29,8 @@ function GetAndShowLiveSuscriptions() {
                 var stremURL = "https://api.twitch.tv/kraken/streams/?channel="+commaSepChannelsIds;
                 streamRequest.open('get',stremURL,true);
                 streamRequest.setRequestHeader("Client-ID","j21jts5r8up5ijbt1pav5ngtb6ctkw");
-                streamRequest.send();
+                streamRequest.send();*/
+            displayStreamsSepByCommas(commaSepChannelsIds);
         };
         
     };
@@ -40,33 +41,79 @@ function GetAndShowLiveSuscriptions() {
 }
 function displayStream(currentStream)
 {
+    var top = document.createElement('div');
+    top.classList.add('positionRelative','overflowAuto','tinyMargin');
+
+    var mid = document.createElement('div');
+    
+    var bot = document.createElement('div');
+    bot.classList.add('marginTop');
+
+    var streamThumb = document.createElement('img');
+    streamThumb.classList.add('widthAll');
+    streamThumb.src = currentStream.preview.large;
+    mid.appendChild(streamThumb);
+
     var image = document.createElement('img');
     image.src = currentStream.channel.logo;
-    image.classList.add('displayInlineBlock','floatLeft','defaultPadding');
-    image.style.height = '50px';
+    image.classList.add('floatLeft', 'marginLeft', 'verticalAlignMiddle');
+    image.style.height = '30px';
+    top.appendChild(image);
+
+    var title = document.createElement('h1');
+    title.innerHTML = currentStream.channel.display_name ;
+    title.classList.add('center', 'xxl', 'floatLeft', 'smallMarginLeft');
+    top.appendChild(title);
+
+    var status = document.createElement('h2');
+    status.innerHTML = currentStream.channel.status;
+    status.classList.add('center', 'm','textEllipsis');
+    bot.appendChild(status);
+    
     var desc = document.createElement('p');
-    desc.classList.add('floatLeft','positionRelative');
-    desc.innerHTML = currentStream.channel.display_name+' stremeando: '+currentStream.game;
+    desc.classList.add('s','textEllipsis');
+    desc.innerHTML = ' stremeando: '+currentStream.game;
+    bot.appendChild(desc);
+
     var watchButton = document.createElement('button');
-    watchButton.innerHTML = 'Ver';
+    watchButton.innerHTML = 'Buscar calidades';
     watchButton.addEventListener('click',function(event) {
         launchLivesream(currentStream,event);
         event.preventDefault();
     });
-    watchButton.classList.add('blueBg','generic_button');
+    watchButton.classList.add('generic_button', 'positionRelative', 'darkGrayBg', 'widthAll');
+    watchButton.id = 'watchButton'+currentStream.channel.name;
+    bot.appendChild(watchButton);
+
+    var loadingGif = document.createElement('img');
+    loadingGif.id = currentStream.channel.name+'LoadingGif';
+    loadingGif.src = 'resources/loading.gif';
+    loadingGif.style.display = 'none';
+    loadingGif.classList.add('clearBorh');
+    loadingGif.style.height = '30px';
+    bot.appendChild(loadingGif);
+
     var element = document.createElement("div");
-    element.classList.add('grid-100','defaultMargin');
-    element.appendChild(image);
-    element.appendChild(desc);
-    element.appendChild(watchButton);
+    element.classList.add('defaultBoxPadding',  'grayBg','floatLeft', 'marginLeft', 'defaultMargin','farElementShadow','white','textEllipsis' );
+    element.style.width = '25%';
+    element.style.backgroundColor = '#6441a4';
+    element.appendChild(top);
+    element.appendChild(mid);
+    element.appendChild(bot);
+    element.id = currentStream.channel.name+'Card';
+    
     var node =document.getElementById('channelGrid');
-    node.classList.add('mContainer','centerBox','maxWidthContainer','doubleMargin');
     node.appendChild(element);    
     
 }
 
 function launchLivesream(currentStream,event)
 {
+    
+    var watchButton = document.getElementById('watchButton'+currentStream.channel.name);
+    watchButton.style.display = 'none';
+    var parentElement = event.srcElement.parentNode;
+    showLoadingGif(currentStream);
     var command = 'streamlink twitch.tv/'+currentStream.channel.name;
     cmd.get(command,function (data,err){
         if (!err)
@@ -75,26 +122,221 @@ function launchLivesream(currentStream,event)
             slicedData = data.slice(pos+18);
             var pat = /\w+/g;
             var calidades = slicedData.match(pat);
-            var node = document.createElement('div');
+            var node = document.createElement('select');
+            node.style.width = '50%';
+            node.style.backgroundColor = '#6441a4';
             calidades.forEach(function (currentQuality){
-                var qualityWatchButton = document.createElement('button');
+                var qualityWatchButton = document.createElement('option');
                 qualityWatchButton.innerHTML = currentQuality;
-                qualityWatchButton.addEventListener('click',function(event){
-                    console.log(command+' '+currentQuality);
-                    cmd.run(command+' '+currentQuality);
-                });
-                qualityWatchButton.classList.add('generic_button')
                 node.appendChild(qualityWatchButton);
             });
+            var verButton = document.createElement('button');
+            verButton.innerHTML = 'Ver stream';
+            verButton.addEventListener('click',function(event){
+                var selector = event.srcElement.parentElement.lastChild;
+                var streamQuality = selector.options[selector.selectedIndex].text;
+                console.log(command+' '+streamQuality);
+                showLoadingGif(currentStream);
+                cmd.run(command+' '+streamQuality);
+                hideLoadingGif(currentStream);
+            });
+            verButton.classList.add('generic_button', 'darkGrayBg', 'widthAll');
+            verButton.style.width = '50%';
+            hideLoadingGif(currentStream);
+            event.srcElement.parentNode.appendChild(verButton);
             event.srcElement.parentNode.appendChild(node);
+
             console.log(event);
 
         }else{
+            alert(data);
             console.log(err);
         }
     });
     console.log(currentStream);
 }
+
+function showLoadingGif(currentStream)
+{
+    var loadingGif = document.getElementById(currentStream.channel.name+'LoadingGif');
+    loadingGif.style.display = 'inline';
+}
+
+function hideLoadingGif(currentStream)
+{
+    var loadingGif = document.getElementById(currentStream.channel.name+'LoadingGif');
+    loadingGif.style.display = 'none';
+}
+
+function displayStreamsSepByCommas(commaSepChannelsIds)
+{
+    var node =document.getElementById('channelGrid');
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+    var streamRequest = new XMLHttpRequest();
+    streamRequest.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            var streamResponse = JSON.parse(this.responseText);
+            console.log(streamResponse);
+            streamResponse.streams.forEach(displayStream);
+        }
+    };
+    var stremURL = "https://api.twitch.tv/kraken/streams/?channel="+commaSepChannelsIds;
+    streamRequest.open('get',stremURL,true);
+    streamRequest.setRequestHeader("Client-ID","j21jts5r8up5ijbt1pav5ngtb6ctkw");
+    streamRequest.send();
+}
+
+function ShowLiveStream()
+{
+     var streams = document.getElementById('streamName').value;
+     displayStreamsSepByCommas(streams);
+}
+
+function displayVod(currentVod)
+{
+    var top = document.createElement('div');
+    top.classList.add('positionRelative','overflowAuto','tinyMargin');
+
+    var mid = document.createElement('div');
+    
+    var bot = document.createElement('div');
+    bot.classList.add('marginTop');
+
+    var streamThumb = document.createElement('img');
+    streamThumb.classList.add('widthAll');
+    streamThumb.src = currentVod.thumbnails[0].url;
+    mid.appendChild(streamThumb);
+
+    var image = document.createElement('img');
+    image.src = 'resources/vod_icon.png';
+    image.classList.add('floatLeft', 'marginLeft', 'verticalAlignMiddle');
+    image.style.height = '30px';
+    top.appendChild(image);
+
+    var title = document.createElement('h1');
+    title.innerHTML = currentVod.channel.display_name ;
+    title.classList.add('center', 'xxl', 'floatLeft', 'smallMarginLeft');
+    top.appendChild(title);
+
+    var status = document.createElement('h2');
+    status.innerHTML = currentVod.description;
+    status.classList.add('center', 'm','textEllipsis');
+    bot.appendChild(status);
+    
+    var desc = document.createElement('p');
+    desc.classList.add('s','textEllipsis');
+    desc.innerHTML = ' stremeando: '+currentVod.game;
+    bot.appendChild(desc);
+
+    var watchButton = document.createElement('button');
+    watchButton.innerHTML = 'Buscar calidades';
+    watchButton.addEventListener('click',function(event) {
+        launchVod(currentVod,event);
+        event.preventDefault();
+    });
+    watchButton.classList.add('generic_button', 'positionRelative', 'darkGrayBg', 'widthAll');
+    watchButton.id = 'watchButton'+currentVod.channel.name;
+    bot.appendChild(watchButton);
+
+    var loadingGif = document.createElement('img');
+    loadingGif.id = currentVod.channel.name+'LoadingGif';
+    loadingGif.src = 'resources/loading.gif';
+    loadingGif.style.display = 'none';
+    loadingGif.classList.add('clearBorh');
+    loadingGif.style.height = '30px';
+    bot.appendChild(loadingGif);
+
+    var element = document.createElement("div");
+    element.classList.add('defaultBoxPadding',  'grayBg','floatLeft', 'marginLeft', 'defaultMargin','farElementShadow','white','textEllipsis' );
+    element.style.width = '25%';
+    element.style.backgroundColor = '#4c2496';
+    element.appendChild(top);
+    element.appendChild(mid);
+    element.appendChild(bot);
+    element.id = currentVod.channel.name+'Card';
+    
+    var node =document.getElementById('channelGrid');
+    node.appendChild(element);    
+    
+}
+
+function launchVod(currentVod,event)
+{
+    
+    var watchButton = document.getElementById('watchButton'+currentVod.channel.name);
+    watchButton.style.display = 'none';
+    var parentElement = event.srcElement.parentNode;
+    showLoadingGif(currentVod);
+    var command = 'streamlink '+currentVod.url;
+    cmd.get(command,function (data,err){
+        if (!err)
+        {
+            var pos = data.indexOf('Available streams:');
+            slicedData = data.slice(pos+18);
+            var pat = /\w+/g;
+            var calidades = slicedData.match(pat);
+            var node = document.createElement('select');
+            node.style.width = '50%';
+            node.style.backgroundColor = '#6441a4';
+            calidades.forEach(function (currentQuality){
+                var qualityWatchButton = document.createElement('option');
+                qualityWatchButton.innerHTML = currentQuality;
+                node.appendChild(qualityWatchButton);
+            });
+            var verButton = document.createElement('button');
+            verButton.innerHTML = 'Ver stream';
+            verButton.addEventListener('click',function(event){
+                var selector = event.srcElement.parentElement.lastChild;
+                var streamQuality = selector.options[selector.selectedIndex].text;
+                console.log(command+' '+streamQuality);
+                showLoadingGif(currentVod);
+                cmd.run(command+' '+streamQuality+' --player-passthrough hls');
+                hideLoadingGif(currentVod);
+            });
+            verButton.classList.add('generic_button', 'darkGrayBg', 'widthAll');
+            verButton.style.width = '50%';
+            hideLoadingGif(currentVod);
+            event.srcElement.parentNode.appendChild(verButton);
+            event.srcElement.parentNode.appendChild(node);
+
+            console.log(event);
+
+        }else{
+            alert(data);
+            console.log(err);
+        }
+    });
+    console.log(currentVod);
+}
+
+function ShowVod()
+{
+    var vodId = document.getElementById('vodId').value;
+    displayVodsSepByCommas(vodId);
+}
+
+function displayVodsSepByCommas(vodId)
+{
+    var node =document.getElementById('channelGrid');
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+    var streamRequest = new XMLHttpRequest();
+    streamRequest.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            var streamResponse = JSON.parse(this.responseText);
+            console.log(streamResponse);
+            displayVod(streamResponse);
+        }
+    };
+    var stremURL = "https://api.twitch.tv/kraken/videos/v"+vodId;
+    streamRequest.open('get',stremURL,true);
+    streamRequest.setRequestHeader("Client-ID","j21jts5r8up5ijbt1pav5ngtb6ctkw");
+    streamRequest.send();
+}
+
 
 
 
